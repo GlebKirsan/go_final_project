2ef6816c9bd30f75f7760847e19e995496826f45
Gleb Kirsanov <dgeehi@gmail.com>
2024-07-26 17:45:22 +0300

    feat: add env port usage

--- internal/pkg/app/app.go
+++ internal/pkg/app/app.go
@@ -2,12 +2,20 @@ package app
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 )
 
 type App struct {
 	router *chi.Mux
+}
+
+func GetEnvOrDefault(key string, def string) string {
+	if variable := os.Getenv(key); len(variable) != 0 {
+		return variable
+	}
+	return def
 }
 
 func New() (*App, error) {
@@ -19,8 +27,9 @@ func New() (*App, error) {
 }
 
 func (a *App) Run() error {
+	port := GetEnvOrDefault("TODO_PORT", "7540")
 	a.router.Handle("/*", http.FileServer(http.Dir("./web")))
-	err := http.ListenAndServe(":7540", a.router)
+	err := http.ListenAndServe(":"+port, a.router)
 	if err != nil {
 		return err
 	}
